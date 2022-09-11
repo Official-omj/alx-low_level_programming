611abc51eb75556fba4e48fa1d61338d8bcb0fe3
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-11 16:06:13 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * 	followed by a new line, to standard error.
+ * followed by a new line, to standard error.
  *
- * 	Return: Always 1.
+ * Return: Always 1.
  */
 int main(void)
 {
@@ -13,5 +13,5 @@ int main(void)
 		59);
 
 
-	return(1);
+	return (1);
 }
