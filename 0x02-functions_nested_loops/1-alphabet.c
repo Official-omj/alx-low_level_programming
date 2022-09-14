2c0e234c452eb3355aa8a48f5e2bd814f9d4e15e
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-13 21:09:23 -0700

    taskt 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
- */
-
+*print_alphabet - functon to print abc
+*
+*Return: 0
+*/
 void print_alphabet(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'zi'; c++)
-	{
-		_putchar(c);
-	}
-	_puchar('\n');
-}
+	char c
+*c
+^c
