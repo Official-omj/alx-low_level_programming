0aaceaef3450410f421b219cf7ba4939f7413384
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-14 05:46:16 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit -function that prints the last digit of a number
  * @n: parameter member n
- * Return: always o if success
+ * Return: 0
  */
 int print_last_digit(int n)
 {
