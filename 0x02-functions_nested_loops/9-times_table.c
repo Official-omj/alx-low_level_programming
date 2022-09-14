cb9a287794af3b62b9e2f6363ad2a0000f767ac0
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-14 11:48:09 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	int product;
 for (row = 0; row <= 9; row++)
 {
-for (column = 0; column <= 9; colum++)
+for (column = 0; column <= 9; column++)
 {
 product = (row * column);
 if (column == 0)
