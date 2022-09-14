afd72dfbdfd8e4e05b26f6fa20d724e995e6a3d5
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-14 12:15:15 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @d: two integer arguments
+ * @b: two integer arguments
+ * Return: sum
+ */
+int add(intb, int d)
+{
+
+	int sum;
+
+	sum = b + d;
+
+	return (sum);
+}
