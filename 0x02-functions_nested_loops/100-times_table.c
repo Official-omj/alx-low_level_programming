439353f428f98d3d370187d0f58a33ffd965468d
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-14 17:34:16 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input, 
+ * print_times_table - Prints the times table of the input,
  * starting from 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(int n)
+void print_times_table(int n);
 {
 	int num, mult, prod;
 
@@ -42,4 +42,3 @@ void print_times_table(int n)
 		}
 	}
 }
-			
