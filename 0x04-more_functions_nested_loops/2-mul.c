6c2019116287ab29d5de95d47e423bb9df87eaae
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-15 20:15:30 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply twi integers
+ * @a: first digit
+ * @b: second digit
+ * Return: return the product
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
