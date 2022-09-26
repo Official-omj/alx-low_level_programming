befdd91fda16fe65bf1d89a31b4e240f7b0d6d6f
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-26 12:32:59 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
-	char *start = s;
-
+	
 	while (i < n)
 	{
-		*s++ = b;
+		s[i] = b;
 		i++;
 	}
-	return (start);
+	return (s);
 }
