23275a271f4e2c7d0eace1e1120c32a7635887c1
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-26 13:33:16 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+
 	unsigned int count = 0;
-	
+
 	for (count = 0; count < n; count++)
 	{
 		*(dest + count) = *(src = count);
 	}
 
 	return (dest);
+
 }
