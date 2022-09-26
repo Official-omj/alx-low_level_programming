7ed5fe4084bd1b9ec62449871bfd5ef89a50bf58
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-09-26 12:26:25 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * _memset - fills memory with constant byte
+ *
  * @s: pointer to the memory area
- * @n: bytes of memory
- * @b: constant byte
+ * @b: constant byte to fill with
+ * @n: num bytes to fill in
  *
  * Return: the memory area of s
  */
