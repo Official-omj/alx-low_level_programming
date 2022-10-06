be72bfce2353758e1625e774f94da65c9c4a8fc5
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-05 22:26:03 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * argstostr - Concatenates all arguments of the program into a string;
  * arguments are separated by a new line in the string.
