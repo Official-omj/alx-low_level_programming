5886a11754a69412d8295b3a75147628bd17d4a3
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-06 17:41:41 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 /**
  * _calloc - allocates memory for an array.
  * @nmemb: number of elements.
- * size: size of bytes.
+ * @size: size of bytes.
  *
  * Return: pointer to the allocated memory.
  * if nmemb or size is 0, returns NULL.
