526f8457b2f1477fe8bd535bfdaff37c0eb2286b
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-06 20:29:54 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,8 +6,6 @@
  * @max: maximum value.
  *
  * Return: pointer to the newly created array.
- * if man > mix, returns NULL.
- * if malloc fails, returns NULL.
  */
 int *array_range(int min, int max)
 {
@@ -30,6 +28,5 @@ int *array_range(int min, int max)
 	{
 		array[i] = min;
 	}
-
 	return (array);
 }
