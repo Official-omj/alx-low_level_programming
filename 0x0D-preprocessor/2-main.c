a3eb8ac8074b2b71cd78a1f977647399e3bc1896
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-10 13:01:49 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - prints the name of the file it was compiled from
- *
- * Return: 0 if successful
+ * Return: Always 0
  */
-int main(voi)
+
+int main(void)
 {
 printf("%s/n", __FILE__);
 return (0);
