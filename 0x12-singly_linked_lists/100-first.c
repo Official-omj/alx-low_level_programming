faba4e6cb626d39027724d78c04aa81391de9136
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-20 20:49:29 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
- * Return: no return.
+ * firstfunc - prints before the main function is executed.
+ *
+ * Return: Always 0.
  */
 
-void _attribute_ ((constructor)) bmain()
+void firstfunc(void) __attribute__ ((constructor));
+
+void firstfunc(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	char *m;
+
+	m = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", m);
 }
