73ed7d865cf9ed61d16ec7118a549b4d5e04b870
Ojelabi Joseph <ojelabijoseph.o@gmail.com>
2022-10-20 19:58:03 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,23 @@
 
 /**
  * print_list - Entry Point
- * @h: head
- * Return: 0
+ * @h: singly linked list
+ * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t nelem;
 
-	if (h == NULL)
-		return (0);
-
+	nelem = 0;
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
+		nelem++;
 	}
-
-	return (count);
+	return (nelem);
 }
+	
